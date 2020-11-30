790df7510e81582efe6c30acbb9fefc72ca6d466
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-11-30 16:42:30 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -64,7 +64,7 @@ namespace aimakeranalogsensors
             return pins.analogReadPin(p);
         }
 
-        //% blockId=isBlackLine block="When black line detected at pin=%p"
+        //% blockId=isBlackLine block="Detect black line  at pin=%p"
         //% group="Laser Distance Sensor"
         export function isBlackLine(p: AnalogPin): boolean {
             if( pins.analogReadPin(p) >= 50 ) {
