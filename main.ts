5409b013f011165d0bd505e918f4997adaa31dc5
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-12-07 09:21:31 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@
  * AIMaker STEM Sensors
  */
 //% color=190 weight=100 icon="\uf1ec" block="AIMaker: analog sensors"
-//% groups=['Sound','Ambient Light Sensor','Flame Sensor', 'Soil Moisture Sensor','Laser Distance Sensor','others']
+//% groups=['Sound','Ambient Light Sensor','Flame Sensor', 'Soil Moisture Sensor','Reflective Infrared Sensor','others']
 namespace aimakeranalogsensors
 {
 /**
  * AIMaker STEM Sensors
  */
 // color=190 weight=100 icon="\uf1ec" block="AIMaker: analog sensors"
-// groups=['Sound','Ambient Light Sensor','Flame Sensor', 'Soil Moisture Sensor','Laser Distance Sensor','others']
+// groups=['Sound','Ambient Light Sensor','Flame Sensor', 'Soil Moisture Sensor','Reflective Infrared Sensor','others']
 
     //% group="Sound"
     export namespace AnalogSound {
@@ -56,16 +56,16 @@ namespace aimakeranalogsensors
         }
     };
 
-    //% group="Laser Distance Sensor"
+    //% group="Reflective Infrared Sensor"
     export namespace LaserDistance {
         //% blockId=trackerValue block="Read tracker value at pin=%p"
-        //% group="Laser Distance Sensor"
+        //% group="Reflective Infrared Sensor"
         export function trackerValue(p: AnalogPin): number {
             return pins.analogReadPin(p);
         }
 
         //% blockId=isBlackLine block="Detected black line  at pin=%p"
-        //% group="Laser Distance Sensor"
+        //% group="Reflective Infrared Sensor"
         export function isBlackLine(p: AnalogPin): boolean {
             if( pins.analogReadPin(p) >= 50 ) {
                 return true;
